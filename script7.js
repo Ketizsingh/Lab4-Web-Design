c5950f4cd99a37b6f1d06fb11efd2a2965f1c597
Ketizsingh <Gurpreettechnologies@gmail.com>
2020-02-03 20:57:14 -0500

    challenge done

--- script7.js
+++ script7.js
@@ -7,20 +7,50 @@ function onEnterPressed(event) {
 }
 
 var nextId = 0;
+// var Names = ['Gurpret','Mohit','Nikhil','Arshdeep','Sharine'];
+var Names = [];
+
 
 function addRow() {
     var name = document.getElementById('name').value;
 
-    var snippet = `
-        <tr id="${nextId}">
-            <td onclick="removeEntry(${nextId})"><img src="images/delete.png"></td>
-            <td>${name}</td>
-        </tr>
-    `;
 
-    document.getElementById('entries').innerHTML += snippet;
+ 
+
+
+    Names.push(name);
+
+    alert('Entry Successful, Press Refresh')
 
     nextId++
+}
+
+
+
+function refreshTable(){
+
+
+    document.getElementById('entries').innerHTML = '';
+
+var code = '';
+
+Names.forEach(name => {
+    
+    var snippet = `
+    <tr id="${nextId}">
+        <td onclick="removeEntry(${nextId})"><img src="images/delete.png"></td>
+        <td>${name}</td>
+    </tr>
+`;
+
+document.getElementById('entries').innerHTML += snippet;
+   
+    nextId++
+
+
+});
+
+
 }
 
 
