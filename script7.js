fedff504ac2813b7139d1032f0a36a41e226f17f
Ketizsingh <Gurpreettechnologies@gmail.com>
2020-02-04 00:41:23 -0500

    Delete Button Working Now.

--- script7.js
+++ script7.js
@@ -14,39 +14,53 @@ var Names = [];
 function addRow() {
     var name = document.getElementById('name').value;
 
-    Names.push(name); //here we push values to 
+    Names.push(name); //here we push values to array
 
-    alert('Entry Successful, Press Refresh')
+    document.getElementById('notification').innerHTML = 'Entry Successful of "'+name+'"'; //entry notification
+
 
     nextId++
 }
 
 
 
-function refreshTable()
+function refreshTable() //execute by press on Refresh Button
 {
     document.getElementById('entries').innerHTML = '';
     var code = '';
-    Names.forEach(name => {
+    
+    for (let index = 0; index < Names.length; index++) {
+        const name = Names[index];
+        
         var snippet = `
             <tr id="${nextId}">
-                <td onclick="removeEntry(${nextId})"><img src="images/delete.png"></td>
+                <td onclick="removeEntry(${nextId},${index})"" ><img src="images/delete.png"></td>
                 <td>${name}</td>
             </tr>
         `;
         document.getElementById('entries').innerHTML += snippet;
         nextId++
-    });
+
+
+    }
+    
 }
 
 
-function removeEntry(Id) 
+function removeEntry(Id,index) 
 {
-    delete Names[Id];
+    Names.splice(index, 1);  //to Names value from array
     document.getElementById(Id).remove()
+
+    document.getElementById('notification').innerHTML = 'Entry Removed Successfully...';
+
+
 }
 
 function cleanEntry() {
     document.getElementById('name').value = ""
 }
+
+
+
 
